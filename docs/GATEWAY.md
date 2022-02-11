acb5787a504d2933619e3f79f2b2142aaa0fd623
tim-oe <tim@outboundengine.com>
2022-02-10 21:22:09 -0600

    node an gateway initial pass

--- docs/GATEWAY.md
+++ docs/GATEWAY.md
@@ -6,6 +6,10 @@ data colection from the hives via [LoRa](https://en.wikipedia.org/wiki/LoRa)
 - [Chirpstack](https://www.chirpstack.io/)
 ## SETUP
 - [RAK instructions](https://docs.rakwireless.com/Product-Categories/WisLink/RAK2245-Pi-HAT/Quickstart/#prerequisites)
-- [manual](https://cdn-shop.adafruit.com/product-files/4284/4283_Pilot_Gateway_Pro_RAK7243_User_Manual_V1.1.pdf)   
+- [manual](https://cdn-shop.adafruit.com/product-files/4284/4283_Pilot_Gateway_Pro_RAK7243_User_Manual_V1.1.pdf)
+- [LoRa-E5 AT command set](https://files.seeedstudio.com/products/317990687/res/LoRa-E5%20AT%20Command%20Specification_V1.0%20.pdf)   
 ## Python libs
 [Pyserial](https://pythonhosted.org/pyserial/)
+## Caveats....
+- mod /etc/dhcpcd.conf
+- mod /etc/dnsmasq.conf
--- monitor/test/py/TestUart.py
+++ monitor/test/py/TestUart.py
@@ -4,30 +4,53 @@ import io
 # test program for seeedstudio LoRa-E5
 # see https://wiki.seeedstudio.com/Grove_LoRa_E5_New_Version/
 
+
 def send(sio, ser, cmd):
     sio.write(cmd)
     sio.flush()
     ser.flush()
 
-ser = serial.Serial("/dev/serial0")  # Open named port
+
+# seems to only work on 9600 baud
+ser = serial.Serial("/dev/serial0", 9600, timeout=60, write_timeout=60)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
-print(ser.name)
+# print(ser.name)
+# print(ser.get_settings())
+# print(ser.BAUDRATES)
 # see https://files.seeedstudio.com/products/317990687/res/LoRa-E5%20module%20datasheet_V1.0.pdf
 send(sio, ser, "AT+VER\n")
+send(sio, ser, "AT+LW=VER\n")
+send(sio, ser, "AT+TEMP\n")
+# send(sio, ser, "AT+LW=LEN\n")
 send(sio, ser, "AT+ID\n")
+send(sio, ser, "AT+POWER\n")
 send(sio, ser, "AT+PORT=?\n")
 send(sio, ser, "AT+ADR=?\n")
 # set to us freg
-# send(sio, ser, "AT+DR=US915\n")
-send(sio, ser, "AT+DR\n")
+send(sio, ser, "AT+DR=US915\n")
+send(sio, ser, "AT+DR=SCHEME\n")
+# enable channels
+send(sio, ser, "AT+CH=NUM, 8-15\n")
 send(sio, ser, "AT+CH=NUM\n")
-# range based on command above
-# for x in range(71):
-#    send(sio, ser, "AT+CH=%d\n" % x)
-# set app key
-# ser.write(str.encode('AT+KEY=APPKEY,"e7565893d479a2a2be7599603de2eed3"'))
+for x in range(8, 16):
+    send(sio, ser, "AT+CH=%d\n" % x)
+# set class
+# send(sio, ser, "AT+CLASS=B,SAVE\n")
+# send(sio, ser, "AT+CLASS\n")
+#
+# set app session keys
+# only do one at a time
+# send(sio, ser, 'AT+KEY=NWKSKEY, "b9 e5 57 0c 98 8e 4e fb f0 32 1d 99 ae 63 c2 06"\n')
+# send(sio, ser, 'AT+KEY=APPSKEY, "4a fe 8f 63 77 30 52 69 10 3f 18 1a e7 63 09 28"\n')
+# send(sio, ser, 'AT+KEY=APPKEY, "c2 11 ae 38 53 3a dc 50 e7 82 c4 2e 14 c4 d4 88"\n')
+#
+# set join mode
+send(sio, ser, "AT+MODE=LWOTAA\n")
+send(sio, ser, "AT+MODE\n")
 
-# TODO connect to gateway, send data packet
+# connect to gateway
+#for x in range(10):
+#    send(sio, ser, "AT+JOIN=FORCE\n")
 
 ser.close()
