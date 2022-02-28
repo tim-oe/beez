746cd674d50bf97f4f0948dcdc48a79241271039
tim-oe <tecronin@gmail.com>
2022-02-28 05:18:11 +0000

    added snesors

--- monitor/src/py/sensor/Humidity.py
+++ monitor/src/py/sensor/Humidity.py
@@ -9,6 +9,24 @@ from Temprature import Temprature
 
 __all__ = ["Humitity"]
 
-
 class Humitity(Temprature):
-    """ """
+    """
+    humidity sensor class
+    see https://shop.switchdoc.com/collections/grove/products/sht30-i2c-waterproof-temperature-and-humidity-sensor-with-grove    
+    """
+
+    def __init__(self, gpioPin=6):
+        """
+        ctor
+        :param self: this
+        :param gpioPin: the gpio pin (ic2)
+        """
+        super(gpioPin)
+        
+    def read(self):
+        """
+        read sensor
+        :param self: this
+        :return: humidity
+        """
+        return self.sensor.read_humidity()
--- monitor/src/py/sensor/Temprature.py
+++ monitor/src/py/sensor/Temprature.py
@@ -9,5 +9,27 @@
 __all__ = ["Temprature"]
 
 
+from py.lib import SHT30
+
+
 class Temprature(object):
-    """ """
+    """
+    temprature sensor class
+    see https://shop.switchdoc.com/collections/grove/products/sht30-i2c-waterproof-temperature-and-humidity-sensor-with-grove    
+    """
+
+    def __init__(self, gpioPin=6):
+        """
+        ctor
+        :param self: this
+        :param gpioPin: the gpio pin (ic2)
+        """
+        self.sensor = SHT30.SHT30(gpioPin)
+        
+    def read(self):
+        """
+        read sensor
+        :param self: this
+        :return: temprature 
+        """
+        return self.sensor.read_temperature()
