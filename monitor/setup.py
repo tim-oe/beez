67be49f1424a217bcb2d9441ea22da472b5b9054
tim-oe <tecronin@gmail.com>
2022-03-13 06:22:56 +0000

    removed bad file fixed sp

--- monitor/setup.py
+++ monitor/setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages, Command
 
 # https://stackoverflow.com/questions/3779915/why-does-python-setup-py-sdist-create-unwanted-project-egg-info-in-project-r
 # to clean cruft : py3clean .
+# p3 setup.py clean
 class CleanCommand(Command):
     """Custom clean command to tidy up the project root."""
 
@@ -21,13 +22,13 @@ class CleanCommand(Command):
 
 
 # https://www.tutorialguruji.com/python/python-package-structure-setup-py-for-running-unit-tests/
-# this is allows to run test in seperate folder
+# run tests 
 # via this script
 # for single test class
-# -s <fully qualified test module>
+# p3 setup tests -s <fully qualified test module>
 setup(
     version="0.1",
-    description="honeyPi beehive montiro",
+    description="honeyPi beehive monitor",
     author="tim Cronin",
     author_email="tecronin@gmail.com",
     packages=find_packages(),
